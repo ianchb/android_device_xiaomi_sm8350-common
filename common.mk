b26885885c0370c6e0ca344d499f7df6348493a0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-06 16:54:40 +0530

    sm8350-common: Import custom light HAL
    We will build this since stock HWC doesn't advertise
    brightness capability and needs forcing in SF [1],
    which isn't available in GSI/DSU.
    
    Also build it correctly and override the default
    lights HAL (lights.qcom) so that our custom one is
    loaded. Switch to bp while we're at it.
    
    This reverts commit e39b4ce3f0dbb10e86fcb1ac3e6236ba52243df8.
    
    [1]: f0c8a66b0feacf0587d6a33c04f02c1ca039ceb2 ("lisa: Force usage of HWC for setting brightness")
    
    Signed-off-by: ianchb <admin@cgtsoft.com>

--- common.mk
+++ common.mk
@@ -327,6 +327,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Lights 
+PRODUCT_PACKAGES += \
+    lights.lahaina
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
