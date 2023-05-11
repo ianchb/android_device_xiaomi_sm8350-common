bc0f0e1821ba8d09682ccb5125c9ed96ce6f7015
EndCredits <endcredits@crepuscular-aosp.icu>
2023-05-11 23:54:52 +0800

    sm8350-common: Build corret { CarrierConfig | Wifi } Res overlay
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- common.mk
+++ common.mk
@@ -452,7 +452,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.6.vendor \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    CarrierConfigOverlay \
+    CarrierConfigResCommon \
     libjson \
     librmnetctl
 
@@ -567,7 +567,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
-    WifiOverlay \
+    WifiResCommon \
     wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf
