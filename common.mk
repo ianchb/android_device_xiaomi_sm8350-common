a2183b66ad6247468828742d564a88c5933b1c24
EndCredits <endcredits@crepuscular-aosp.icu>
2023-10-21 09:36:35 +0800

    sm8350-common: Provide drm vendor HIDL interface
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>
    Signed-off-by: ianchb <admin@cgtsoft.com>

--- common.mk
+++ common.mk
@@ -199,7 +199,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
